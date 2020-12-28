bcf8c4f30a59a487a5efbe22edd2d0bef0626b89
ayberk <mralbino54@hotmail.com>
2020-12-28 23:57:03 +0300

    pagination update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Prog_Proje.Data;
 using Web_Prog_Proje.Models;
 
 namespace Web_Prog_Proje.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context= context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var db = _context.Kitap.Include(i => i.Dil).Include(i => i.Kategori);
+            return View(db.ToList());
         }
 
         public IActionResult Privacy()
